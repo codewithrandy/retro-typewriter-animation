3adf0d95aadd5491f374f85f30f8c7006d317ed4
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2022-05-02 11:31:05 -0600

    init commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Retro Typewriter Animation Effect in Python
+Learn how to code a retro typewriter animation console effect in Python. This is a Python for beginners tutorial.
+
+Watch the tutorial on YouTube at https://youtu.be/8Yd-JGeYCs8
---
+++ typewriter_fx.py
@@ -0,0 +1,13 @@
+### WATCH THE VIDEO TUTORIAL ON MY YOUTUBE CHANNEL
+### https://youtu.be/8Yd-JGeYCs8
+
+from time import sleep
+
+def type(text):
+	for i in text:
+		print(i, end = '')
+		sleep(.1)
+	print('')
+
+type('SHALL WE PLAY A GAME?')
+type('HOW ABOUT A NICE GAME OF CHESS')
